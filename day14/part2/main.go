3a4086ece03962577488209d33deaf42f264e8d8
Thomas Gorham <tgiv014@gmail.com>
2020-12-14 22:11:15 -0600

    Day 14 part 2

--- day14/part2/main.go
+++ day14/part2/main.go
@@ -5,16 +5,77 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
-	"strings"
+)
+
+var (
+	reMask *regexp.Regexp
+	reMem  *regexp.Regexp
 )
 
 type input struct {
-	earliest int64
-	busses   []int64
-	offsets  []int64
+	mask_high uint64
+	mask_x    []int
+	mem       map[uint64]uint64
 }
 
+func (in *input) build_mask(m []byte) {
+	in.mask_high = uint64(0)
+	in.mask_x = make([]int, 0) // Reset X mask
+	// Loops from start of string to end
+	// We will switch bitshift around to make sense here
+	for i, b := range m {
+		n := len(m) - i - 1 // Actual bit index
+		switch b {
+		case '0':
+			continue
+		case '1':
+			in.mask_high |= (1 << n)
+		case 'X':
+			in.mask_x = append(in.mask_x, n)
+		default:
+			fmt.Println("Invalid input")
+		}
+	}
+}
+
+func (in *input) apply(addr uint64, data uint64) {
+	var r uint64 = addr
+	r |= in.mask_high // Still apply highs
+
+	// Consider I a "len(in.mask_x)"-bit wide counter
+	for i := 0; i < (1 << len(in.mask_x)); i++ {
+		for j, v := range in.mask_x {
+			if i&(1<<j) != 0 {
+				r |= (1 << v)
+			} else {
+				r &= ^(1 << v)
+			}
+		}
+		in.mem[r] = data
+	}
+}
+
+func (in *input) sum() uint64 {
+	var t uint64 = 0
+	for _, val := range in.mem {
+		t += val
+	}
+	return t
+}
+
+func init() {
+	var err error
+	reMask, err = regexp.Compile("^mask = ([01X]+)$")
+	if err != nil {
+		log.Fatal(err)
+	}
+	reMem, err = regexp.Compile("^mem\\[([0-9]+)\\] = ([0-9]+)$")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 func main() {
 	f, err := os.Open("./input")
 	if err != nil {
@@ -22,69 +83,36 @@ func main() {
 	}
 	defer f.Close()
 
-	in := input{0, make([]int64, 0), make([]int64, 0)}
+	in := input{0, make([]int, 0), make(map[uint64]uint64)}
 
 	scanner := bufio.NewScanner(f)
 	for i := 0; scanner.Scan(); i++ {
 		line := scanner.Text()
-		if i == 0 {
-			earliest, err := strconv.Atoi(line)
+
+		maskMatch := reMask.FindSubmatch([]byte(line))
+		if maskMatch != nil {
+			in.build_mask(maskMatch[1])
+			continue
+		}
+
+		memMatch := reMem.FindSubmatch([]byte(line))
+		if memMatch != nil {
+			addrI, err := strconv.ParseUint(string(memMatch[1]), 10, 64)
 			if err != nil {
-				log.Fatal(err)
+				fmt.Println(err)
 			}
-			in.earliest = int64(earliest)
-		}
-		if i == 1 {
-			substrings := strings.Split(line, ",")
-			for j, substring := range substrings {
-				if substring == "x" {
-					continue
-				}
-				bus, err := strconv.Atoi(substring)
-				if err != nil {
-					log.Fatal(err)
-				}
-				in.busses = append(in.busses, int64(bus))
-				in.offsets = append(in.offsets, int64(j))
+			dataI, err := strconv.ParseUint(string(memMatch[2]), 10, 64)
+			if err != nil {
+				fmt.Println(err)
 			}
+			in.apply(addrI, dataI)
+			continue
 		}
+		fmt.Println("Parser missed a line :(")
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	// Data is read in
-	fmt.Println(in)
-
-	// Consider:
-	// We're looking for the solution t to the system of equations
-	// 0 = (t + offsets[i]) % busses[i]
-	// for i = [0:len(busses)]
-	//
-	// This solution works by finding the earliest occurence and
-	// period of solutions for 2 busses, then 3 busses, and so on...
-	//
-	// This works because we know that the solution for n busses
-	// has to occur at one of the times that works for n-1 busses as well.
-	//
-	// This dramatically lowers the size of the search space
-
-	repeat := int64(in.busses[0])
-	t := int64(0)
-	for busidx := 1; busidx < len(in.busses); busidx++ {
-		finds := 0
-		for time := t; finds < 2; time += repeat {
-			bus := in.busses[busidx]
-			if (time+in.offsets[busidx])%bus == 0 {
-				// Success!
-				if finds == 0 {
-					t = time
-				} else if finds == 1 {
-					repeat = time - t
-				}
-				finds++
-			}
-		}
-	}
-	fmt.Println(t)
+	fmt.Println("Sum:", in.sum())
 }

bdc33d991cc5e1ffe8a372633e368a3eee6f411d
Thomas Gorham <tgiv014@gmail.com>
2020-12-21 12:28:48 -0600

    Add day 21 solution

---
+++ day21/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type ingredient struct {
+	name             string
+	nFoodsIn         int
+	allergenMentions map[string]int
+	all              *allergen
+}
+
+type allergen struct {
+	name               string
+	ingredientMentions map[string]int
+	ing                *ingredient
+}
+
+type food struct {
+	ingredients []*ingredient
+	allergens   []*allergen
+}
+
+var reIngredients *regexp.Regexp
+
+func init() {
+	reIngredients = regexp.MustCompile(`^([a-z ]+) \(contains ([a-z ,]+)\)$`)
+}
+func main() {
+	f, err := os.Open("./input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	foods := make([]*food, 0)
+	allIngredients := make(map[string]*ingredient)
+	allAllergens := make(map[string]*allergen)
+
+	// Read in the seed
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		m := reIngredients.FindSubmatch(scanner.Bytes())
+		primaryIngredients := strings.Split(string(m[1]), " ")
+		allergens := strings.Split(string(m[2]), ", ")
+		myIngredients := []*ingredient{}
+		myAllergens := []*allergen{}
+		for _, ing := range primaryIngredients {
+			// Only insert new ingredient to the `all` list if needed
+			if _, ok := allIngredients[ing]; !ok {
+				allIngredients[ing] = &ingredient{ing, 0, make(map[string]int), nil}
+			}
+			myIngredients = append(myIngredients, allIngredients[ing])
+			allIngredients[ing].nFoodsIn++
+		}
+
+		for _, all := range allergens {
+			// Only insert new ingredient to the `all` list if needed
+			if _, ok := allAllergens[all]; !ok {
+				allAllergens[all] = &allergen{all, make(map[string]int), nil}
+			}
+			myAllergens = append(myAllergens, allAllergens[all])
+		}
+		newFood := food{myIngredients, myAllergens}
+		foods = append(foods, &newFood)
+	}
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+
+	for _, fd := range foods {
+		for _, ing := range fd.ingredients {
+			for _, allergen := range fd.allergens {
+				ing.allergenMentions[allergen.name]++
+			}
+		}
+	}
+	for _, fd := range foods {
+		for _, allergen := range fd.allergens {
+			for _, ing := range fd.ingredients {
+				allergen.ingredientMentions[ing.name]++
+			}
+		}
+	}
+
+	nFound := 0
+	for nFound < len(allAllergens) {
+		for _, all := range allAllergens {
+			if all.ing != nil {
+				continue
+			}
+			// Get max number of mentions
+			max := 0
+			for _, v := range all.ingredientMentions {
+				if v > max {
+					max = v
+				}
+			}
+			// See if max happens only once
+			nMaximums := 0
+			for _, v := range all.ingredientMentions {
+				if v == max {
+					nMaximums++
+				}
+			}
+			// Very likely that we've found the correct match now
+			if nMaximums == 1 {
+				nFound++
+				for k, v := range all.ingredientMentions {
+					if v == max {
+						all.ing = allIngredients[k]
+						allIngredients[k].all = all
+						// Delete the newly found ingredient from other allergens mention lists
+						for ak, av := range allAllergens {
+							if ak == all.name {
+								continue
+							}
+							delete(av.ingredientMentions, k)
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+	for _, all := range allAllergens {
+		fmt.Println(all.name, "->", all.ing.name)
+	}
+
+	nSafeMentions := 0
+	unsafeIngredients := []*ingredient{}
+	for _, ing := range allIngredients {
+		if ing.all == nil {
+			nSafeMentions += ing.nFoodsIn
+		} else {
+			unsafeIngredients = append(unsafeIngredients, ing)
+		}
+	}
+	fmt.Println("Safe Mentions:", nSafeMentions)
+	sort.Slice(unsafeIngredients, func(i, j int) bool {
+		return unsafeIngredients[i].all.name < unsafeIngredients[j].all.name
+	})
+	unsafeIngredientNames := []string{}
+	for _, ing := range unsafeIngredients {
+		unsafeIngredientNames = append(unsafeIngredientNames, ing.name)
+	}
+	fmt.Println("Canonical Unsafe Ingedients:", strings.Join(unsafeIngredientNames, ","))
+}
